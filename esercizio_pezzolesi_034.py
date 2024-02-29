354b4d4632069e560eedb2f0e865c25942a7e3f7
pezzolesi <lorenzo.pezzolesi@studenti.isissgobetti.it>
2024-02-29 12:35:20 +0100

    file

---
+++ esercizio_pezzolesi_034.py
@@ -0,0 +1,23 @@
+#1) Leggi il file in input
+#2) Calcola il prezzo scontato e aggiungetelo al dizionario come nel precedente esercizio
+#3) Ordina le fatture in ordine decrescente con il prezzo scontato come criterio di ordinamento
+#4) Salva la lista nel file output_json.json
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
