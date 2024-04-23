dc47044eae584841aece9d19f4d49ffc79bdd52c
pezzolesi <lorenzo.pezzolesi@studenti.isissgobetti.it>
2024-04-23 10:50:23 +0200

    x

--- pezzolesi_es_037.py
+++ pezzolesi_es_037.py
@@ -1,50 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 import json
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return ""
-
-
-@app.route("/dati_grezzi/")
-def dati_grezzi():
-    with open("votes.json") as f:
+    with open("esercizio_037.json") as f:
         data = json.load(f)
-    return data
-
-@app.route("/dati_formattati/")
-def dati_formattati():
-    with open("votes.json") as f:
-        data = json.load(f)
-    result = ''
-    for student in data:
-        result += f"{student['name']} ha preso {student['vote']}"
-        result += "<br>"
-    return result
-
-
-@app.route("/table/")
-def table():
-    with open("votes.json") as f:
-        data = json.load(f)
-    return render_template("table.html", students=data)
+    return render_template("dinamic.html",links = data)
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=1234)
-
-#def carica_il_file():
-
-
-
-
-
-
-
-
-
-
-
-
+    app.run(debug=True, port=6946)
